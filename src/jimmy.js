0c2998386286d2f15a700b2995b847e58ca8e36a
Brendan Le Tourneau <brendanlet@gmail.com>
2018-05-27 00:25:45 -0700

    Made runner do waiting

--- src/jimmy.js
+++ src/jimmy.js
@@ -1,2 +1,19 @@
-jimmy = 5;
-console.log(jimmy);
+jimmy = 0;
+while(jimmy < 25)
+{
+	jimmy++;
+	sleep(1000);
+	console.log(jimmy);
+
+	
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
