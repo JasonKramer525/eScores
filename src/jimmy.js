43f64263fc82f6099838ec257a9b0b289dbaa2b1
Brendan Le Tourneau <brendanlet@gmail.com>
2018-05-31 23:51:26 -0700

    HTML Sorts don't scroll any more

--- src/jimmy.js
+++ src/jimmy.js
@@ -1,7 +1,6 @@
 
-$.ajax({
+console.log($.ajax({
             url: "https://api.fortnitetracker.com/v1/profile/pc/Ninja",
             type: 'GET',
-            crossDomain: true,
             headers: {"TRN-Api-Key": "8d06d9a4-2e55-45ac-a3aa-dc49e7797ee6"}
-      });
+      }));
