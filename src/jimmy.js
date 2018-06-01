f1cc216c4654b6c50bc038a7f7e5e54e6b613d4f
Brendan Le Tourneau <brendanlet@gmail.com>
2018-05-31 23:13:21 -0700

    Don't worry about jimbo

--- src/jimmy.js
+++ src/jimmy.js
@@ -1,7 +1,7 @@
 
-
 $.ajax({
-            url: "https://api.fortnitetracker.com/v1/profile/pc/ninja",
+            url: "https://api.fortnitetracker.com/v1/profile/pc/Ninja",
+            type: 'GET',
+            crossDomain: true,
             headers: {"TRN-Api-Key": "8d06d9a4-2e55-45ac-a3aa-dc49e7797ee6"}
-
       });
