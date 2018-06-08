aeca64b8b42af61e64488f6a67af162cf3c573c5
Jason Kramer <jasonkramer525@gmail.com>
2018-06-08 04:31:27 -0700

    New PHP File

--- src/fortnite.php
+++ src/fortnite.php
@@ -2,12 +2,12 @@
 $fp = fopen("dashboard.txt", "w");
 file_put_contents("text.txt", "");
 
-$names = array("ninja", "KingRichard15", "TSM_Daequan", "Tramear", "Mr. Smile More", "CDNThe3rd", "TheDoritoKilla", "coL%20Hogman", "DolanDark_", "ItsCizzorz", "ValkyraeXD", "xd McCreamy", "xd Zuckles", "ChicaLive", "FaZe%20SpaceLyon ", "KYRSP33DY", "sidearmsbruh", "Ninja", "CourageJD", "2xChampion", "Kraftyyz", "XxGhostNinja187", "DooM%20Bigfoltz", "Dark", "Yelo", "ImKeithCozart", "KennySoom", "OpTic Baldy", "OpTic Dramas", "Niick28T", "NepentehZ", "100T%20Nadeshot", "Kenith", "ChancesFrost", "TheJoshOG", "Typical%20Gamer", "thiefs", "BobbyBoJanglles", "bchillz", "NotNoahJ456", "Avxry", "FaZe%20Tfue", "FaZe%20cLoak", "Nick%20Eh%2030", "ONE_shot_gurl", "ItsMeVikkstar123", "TwitchTinny", "haroldbog17", "theburntchipp", "fazey_banks", "BOT%20Tênnp0", "drlupo", "imtimthetatman", "JoogSquad69", "Fnatic_Ettnix", "NICKMERCS", "SypherPK", "TheTBNRfrags", "LachyDachy", "ComedyShortsDeji", "Wizzite", "TSM_Myth", "TSM_Hamlinz");
-$console = array();
+$names = array("pegasus2012", "KingRichard15", "TSM_Daequan", "Tramear", "Mr. Smile More", "CDNThe3rd", "TheDoritoKilla", "coL%20Hogman", "DolanDark_", "ItsCizzorz", "ValkyraeXD", "xd McCreamy", "xd Zuckles", "ChicaLive", "FaZe%20SpaceLyon ", "KYRSP33DY", "sidearmsbruh", "Ninja", "CourageJD", "2xChampion", "Kraftyyz", "psn(XxGhostNinja187)", "DooM%20Bigfoltz", "Dark", "Yelo.", "ImKeithCozart", "KennySoom", "OpTic Baldy", "OpTic Dramas", "Niick28T", "NepentehZ", "100T%20Nadeshot", "Kenith", "ChancesFrost", "TheJoshOG", "Typical%20Gamer", "thiefs", "BobbyBoJanglles", "bchillz", "NotNoahJ456", "Avxry", "FaZe%20Tfue", "FaZe%20cLoak", "Nick%20Eh%2030", "ONE_shot_gurl", "ItsMeVikkstar123", "TwitchTinny", "haroldbog17", "theburntchipp", "B%CE%91%CE%9D%CE%9AS", "BOT%20T%C3%AAnnp0", "drlupo", "imtimthetatman", "JoogSquad69", "Fnatic_Ettnix", "NICKMERCS", "SypherPK", "TheTBNRfrags", "LachyDachy", "ComedyShortsDeji", "Wizzite", "TSM_Myth", "TSM_Hamlinz");
+$console = array("pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "psn", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "psn", "psn", "pc", "pc", "psn", "psn", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "pc", "psn", "pc", "pc", "pc", "pc", "pc", "psn", "pc", "pc", "pc", "pc", "pc", "pc", "pc",);
 
 for($i=0;$i<count($names);$i++){
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.fortnitetracker.com/v1/profile/pc/".$names[$i]."/");
+curl_setopt($ch, CURLOPT_URL, "https://api.fortnitetracker.com/v1/profile/".$console[$i]."/".$names[$i]."/");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
